11b0e2f04f3ff4df016b73deeed5de084a89d893
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 14:16:58 +0300

    added to models.py

--- server/models.py
+++ server/models.py
@@ -2,41 +2,47 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 
-from server.config import db, bcrypt
+from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    _password_hash = db.Column('password_hash', db.String, nullable=False)
-    bio = db.Column(db.Text)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
-    recipes = db.relationship('Recipe', backref='user')
+    recipes = db.relationship('Recipe', back_populates='user')
+
+    serialize_rules = ('-recipes.user', '-_password_hash')
 
     @hybrid_property
     def password_hash(self):
-        raise AttributeError("password_hash is not a readable attribute")
+        raise AttributeError('password_hash is not accessible')
 
     @password_hash.setter
-    def password_hash(self, plaintext_password):
-        self._password_hash = bcrypt.generate_password_hash(plaintext_password).decode('utf-8')
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    def authenticate(self, plaintext_password):
-        return bcrypt.check_password_hash(self._password_hash, plaintext_password)
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    instructions = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
     minutes_to_complete = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    user = db.relationship('User', back_populates='recipes')
+
+    serialize_rules = ('-user.recipes',)
+
     @validates('instructions')
     def validate_instructions(self, key, instructions):
-        if not instructions or len(instructions) < 50:
-            raise ValueError('instructions must be at least 50 characters')
+        if len(instructions) < 50:
+            raise ValueError('Instructions must be at least 50 characters long')
         return instructions
