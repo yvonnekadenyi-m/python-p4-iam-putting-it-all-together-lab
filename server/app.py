22c633ea8a01fbbe8f1249fd4474ff161b033b32
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 20:01:43 +0300

    added to app.py

--- server/app.py
+++ server/app.py
@@ -55,7 +55,7 @@ class CheckSession(Resource):
         user_id = session.get("user_id")
 
         if user_id:
-            user = User.query.get(user_id)
+            user = db.session.get(User, user_id)
             return user.to_dict(), 200
 
         return {"error": "Unauthorized"}, 401
